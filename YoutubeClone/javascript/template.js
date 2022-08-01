e3a2889a0efe3db8b720ce53167ab498fb5734de
Tushar Sharma <tusharsharmaek@gmail.com>
2022-08-01 13:15:03 +0530

    Update template.js

--- YoutubeClone/javascript/template.js
+++ YoutubeClone/javascript/template.js
@@ -8,7 +8,7 @@ const allVideos = database();
 // Homepage
 
 document.getElementsByClassName("header-item1")[0].addEventListener("click", () => {
-    window.location.replace("homepage.html");
+    window.location.replace("./index.html");
 })
 
 
