1de09ff257ab5932676f47294af8e8fc32c0270b
Tushar Sharma <tusharsharmaek@gmail.com>
2022-08-10 18:45:45 +0530

    Update todo.js

--- AdvanceTodoList/todo.js
+++ AdvanceTodoList/todo.js
@@ -26,6 +26,16 @@ function renderAllThetasks() {
             const deleteSpan = document.createElement("span");
             const updateSpan = document.createElement("span");
 
+            // Adding eventListener to hover and toggle the class
+            containerForSpans.setAttribute("draggable", "true");
+            containerForSpans.setAttribute("ondragstart", "drag(event)");
+
+            // Fetching the DropBox
+            const DropBox = document.getElementsByClassName("dropbox")[0];
+            DropBox.setAttribute("ondrop", "drop(event)");
+            DropBox.setAttribute("ondragover", "allowDrop(event)");
+
+
             // create Text node for spans
             const titleText = document.createTextNode(element["title"]);
             const taskText = document.createTextNode(element["task"])
@@ -51,7 +61,8 @@ function renderAllThetasks() {
             deleteSpan.classList.add("material-symbols-outlined");
             updateSpan.classList.add("material-symbols-outlined");
             deleteSpan.classList.add("deleteIcon");
-            updateSpan.classList.add("updateIcon")
+            updateSpan.classList.add("updateIcon");
+            
             
 
             // Binding them togather
@@ -72,7 +83,6 @@ function renderAllThetasks() {
 
 
             // Adding eventListener for delete and update
-
             const updateIcon = updateSpan;
             const deleteIcon = deleteSpan;
 
@@ -81,12 +91,28 @@ function renderAllThetasks() {
                 const elementToUpdate = containerForSpans.className;
                 updateButton(elementToUpdate);
             })
+
+            updateIcon.addEventListener("mouseover", ()=>{
+                updationIndication(containerForSpans);
+            })
+
+            updateIcon.addEventListener("mouseout", ()=>{
+                updationIndication(containerForSpans);
+            })
+
             deleteIcon.addEventListener('click', ()=>{
                 // calling delete function to delete things
                 const elementToDel = containerForSpans.className;
                 deleteButton(elementToDel);
             })
 
+            deleteIcon.addEventListener("mouseover", ()=>{
+                deletionIndication(containerForSpans);
+            })
+
+            deleteIcon.addEventListener("mouseout", ()=>{
+                deletionIndication(containerForSpans);
+            })
 
         })
     }
@@ -97,7 +123,6 @@ function renderAllThetasks() {
 // Function Second(2) To Add the task into the LocalStorage--------------------------
 
 function addTheTask() {
-    console.log("is it working")
     let tempArray = [];
     // fetch the data from the inputs
     const title = document.getElementById("title").value;
@@ -136,9 +161,9 @@ function deleteButton(element) {
     const storedInfo = localStorage.getItem(databaseName);
     // converting
     const parsedArr = JSON.parse(storedInfo);
-
     const elementToDelete = parsedArr[parseInt(element.split(" ")[1])];
-
+    const elementFromDom = document.getElementsByClassName(element.split(" ")[1]);
+    elementFromDom[0].classList.add("remove-item")
     // Filter the array now with filter method
     const filteredArray = parsedArr.filter((x)=>{
         return x !== elementToDelete;
@@ -146,14 +171,16 @@ function deleteButton(element) {
 
     let jsonArr = JSON.stringify(filteredArray);
     localStorage.setItem(databaseName, jsonArr)
-    location.reload();
+    setTimeout(() => {
+        location.reload();
+    }, 500);
 }
 
 
 
 // Function Fourth(4) To update the task and save it to the localStorage --------------------------
 
-let temp = [];
+let temp = [];  
 
 function updateTheTask() {
     const storedInfo = localStorage.getItem(databaseName);
@@ -184,6 +211,29 @@ function updateTheTask() {
 }
 
 
+
+// Making drag working-------------------------------------
+
+// On Drag Start
+
+let tempdrag;
+
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+function drag(ev) {
+    tempdrag = ev.target;
+}
+
+function drop(ev) {
+ev.preventDefault();
+const dropbox = document.getElementsByClassName('dropbox')[0];
+dropbox.appendChild(tempdrag);
+tempdrag = "";
+}
+
+
 // Small Functions----------------------------------
 
 
@@ -192,7 +242,7 @@ function updateTheTask() {
 function updateButton(element) {
     // Fetching the Editable window to make it block
     const editableWindowSection = document.getElementsByClassName("editable")[0];
-    editableWindowSection.style.display = "block";
+    editableWindowSection.style.display = "flex";
 
     // Fetching the editableWindow
     const editableWindow = document.getElementById("editable-input");
@@ -226,3 +276,29 @@ function hideTitle(hide, show) {
     hideEelment.style.display = "none"
     showElement.style.display = "flex"
 }
+
+
+// To add the class will indicate the delete action
+function deletionIndication(element){
+    let splitting = element.classList[1];
+    const elementToFetch = document.getElementsByClassName(splitting)[0];
+    elementToFetch.classList.toggle("deletionIndication");
+}
+
+// To remove the class will indicate the delete action
+function updationIndication(element){
+    let splitting = element.classList[1];
+    const elementToFetch = document.getElementsByClassName(splitting)[0];
+    elementToFetch.classList.toggle("updationIndication");
+}
+
+
+// ToggleDropBox-----------------
+
+function toggleDropBox() {
+    const dropbox = document.getElementsByClassName("dropbox")[0];
+    const menuTrigger = document.getElementsByClassName("menu-trigger")[0];
+    dropbox.classList.toggle("toggleDropBox");
+    menuTrigger.classList.toggle("rotate-it");
+}
+
