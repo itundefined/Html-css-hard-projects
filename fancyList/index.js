c9b86bbe3bb0f963146da82cf68266e721b53a6d
Tushar Sharma <tusharsharmaek@gmail.com>
2022-11-26 16:46:11 +0530

    Add files via upload

--- fancyList/index.js
+++ fancyList/index.js
@@ -1,165 +1,245 @@
-(function(){
+(function () {
+  class TodoListHelper {
+    constructor() {
+      const databaseName = "testing1"; //localDatabse name connected to the localStorage
+      const localdatabase = JSON.parse(localStorage.getItem(databaseName));
 
-    class TodoListHelper{
-        constructor(){
+      this.databasename = databaseName;
 
-            const databaseName = "testing1" //localDatabse name connected to the localStorage
-            const localdatabase = JSON.parse(localStorage.getItem(databaseName));
+      this.localdatabase = localdatabase;
 
-            this.databasename = databaseName;
+      if (localdatabase) {
+        this.renderAllTasks(databaseName);
+      } else {
+        localStorage.setItem(databaseName, JSON.stringify([]));
+      }
 
-
-            this.localdatabase = localdatabase;
-
-            if(localdatabase) {
-                this.renderAllTasks(databaseName);
-            }
-            else{
-                localStorage.setItem(databaseName, JSON.stringify([]))
-            }
-            
-            
-            this.AllEventListeners();
-        }
-
-        databaseRead() {
-            const oldData = localStorage.getItem(this.databasename);
-
-            // converting
-            const parsedArr = JSON.parse(oldData);
-
-            return parsedArr;
-        }
-
-        databaseWrite(yourObj) {
-            // Creating the task object 
-            const taskAndTitle = {"title":yourObj.keyNote.value, "task":yourObj.descriptionText.value, "id":yourObj.id};
-
-
-            //checking whether if there is some data previously
-    
-            const oldData = localStorage.getItem(this.databasename);
-    
-            // converting
-            const parsedArr = JSON.parse(oldData);
-
-            let tempArray = [];
-
-            if(parsedArr) {
-                tempArray.push(...parsedArr);
-                tempArray.push(taskAndTitle);
-                let jsonArr = JSON.stringify(tempArray);
-                localStorage.setItem(this.databasename, jsonArr)
-            }
-            else{
-                tempArray.push(taskAndTitle);
-                let jsonArr = JSON.stringify(tempArray);
-                localStorage.setItem(this.databasename, jsonArr)
-            }
-
-            this.renderAllTasks();
-        }
-
-
-        createUniqueId() {
-            return Date.now();
-        }
-
-        pageTransition() {
-        }
-
-        AllEventListeners() {
-
-        const that = this;
-        const [today, allOtherDay, createNew] = document.querySelectorAll(".menuList li");
-
-
-        createNew.onclick = function(){
-            that.pageTransition();
-            const createUpdateWindow = document.querySelector(".createUpdate");
-            const buttonToCreate = document.querySelector(".updateOrDelete");
-            buttonToCreate.innerText = "Create";
-            buttonToCreate.onclick = function(){that.addTask()};
-            createUpdateWindow.style.display = "flex"};
-        };
-
-        renderAllTasks() {
-            const taskContainer = document.querySelector(".TasksList");
-            taskContainer.innerHTML = "";
-
-            const allTasks = this.databaseRead();
-
-            const niceBackground = ["background-image: linear-gradient(120deg, #a6c0fe 0%, #f68084 100%);", "background-image: linear-gradient(to right, #ffecd2 0%, #fcb69f 100%);","background-image: linear-gradient(to right, #ffecd2 0%, #fcb69f 100%);", "background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);"] 
-            let wheel = -1;
-
-            allTasks.map((item)=>{
-                wheel = wheel + 1;
-                const colorToChoose = niceBackground[wheel];
-                if(wheel >= niceBackground.length) {
-                    wheel = -1;
-                }
-                console.log(wheel);
-                const html = `   <div class="card ${item.id}" style="${colorToChoose}">
-                                    <div class="cardWrapper">
-                                        <p class="title">${item.title}</p>
-                                        <span class="material-symbols-outlined">more_horiz</span>
-                                    </div>
-                                    <p>Read More.</p>
-                                </div>
-                            `;
-                taskContainer.insertAdjacentHTML("afterbegin", html);
-            });
-
-        };
-
-        addTask() {
-            const createUpdateWindow = document.querySelector(".createUpdate");
-            const [keyNote, descriptionText] = document.querySelectorAll(".inputs");
-
-            let verify = descriptionText.value.split("");
-            let verify2 = keyNote.value.split("");
-
-            verify = verify.filter((x)=>{
-                if(x !== " "){
-                    return x;
-                }
-            })
-
-
-            verify2 = verify2.filter((x)=>{
-                if(x !== " "){
-                    return x;
-                }
-            })
-
-
-            if(verify.length !== 0 && verify2.length !== 0) {
-                // Unique Id
-                const id = this.createUniqueId();
-
-                // Creating the task object 
-                const taskAndTitle = {"keyNote":keyNote, "descriptionText":descriptionText, "id":id};
-
-                this.databaseWrite(taskAndTitle);
-               
-                keyNote.value = "";
-                descriptionText.value = "";
-                createUpdateWindow.style.display = "none";
-            }
-
-            else{
-                alert("Please fill all fields")
-            }
-
-        };
-
-        removeTask() {};
-
-        updateTask() {};
-
-        sortTask() {};
+      this.AllEventListeners();
     }
 
-    const trigger = new TodoListHelper();
+    databaseRead() {
+      const oldData = localStorage.getItem(this.databasename);
 
+      // converting
+      const parsedArr = JSON.parse(oldData);
+
+      return parsedArr;
+    }
+
+    databaseWrite(yourObj) {
+      // Creating the task object
+      const taskAndTitle = {
+        title: yourObj.keyNote.value,
+        task: yourObj.descriptionText.value,
+        id: yourObj.id,
+      };
+
+      //checking whether if there is some data previously
+
+      const oldData = localStorage.getItem(this.databasename);
+
+      // converting
+      const parsedArr = JSON.parse(oldData);
+
+      let tempArray = [];
+
+      if (parsedArr) {
+        tempArray.push(...parsedArr);
+        tempArray.push(taskAndTitle);
+        let jsonArr = JSON.stringify(tempArray);
+        localStorage.setItem(this.databasename, jsonArr);
+      } else {
+        tempArray.push(taskAndTitle);
+        let jsonArr = JSON.stringify(tempArray);
+        localStorage.setItem(this.databasename, jsonArr);
+      }
+
+      this.renderAllTasks();
+    }
+
+    databaseUpdate(yourObj){
+        let tempArray = yourObj;
+        let jsonArr = JSON.stringify(tempArray);
+        localStorage.setItem(this.databasename, jsonArr);
+    }
+
+    createUniqueId() {
+      return Date.now();
+    }
+
+    pageTransition() {
+        const pageChanger = document.querySelector(".pageChanger");
+        pageChanger.classList.toggle("stepPage");
+    }
+
+    AllEventListeners() {
+      const that = this;
+      const [today, allOtherDay, createNew] =
+        document.querySelectorAll(".menuList li");
+
+        const createUpdateWindow = document.querySelector(".createUpdate");
+        const buttonToCreate = document.querySelector(".updateOrDelete");
+
+        const MainCards = document.querySelector(".TasksList");
+
+
+        MainCards.addEventListener("click", function(e){
+            if(e.target.classList.contains("updateButton")) {
+                that.updateTask(e.target.classList);
+                buttonToCreate.innerText = "update";
+
+
+                setTimeout(() => {
+                    createUpdateWindow.style.display = "flex";
+                }, 800);
+            }
+
+            else if(e.target.classList.contains("deleteButton")) {
+                that.removeTask(e.target.classList);
+            }
+        })
+
+      createNew.onclick = function () {
+        that.pageTransition();
+        buttonToCreate.innerText = "Create";
+        buttonToCreate.onclick = function () {
+          that.addTask();
+        };
+
+        setTimeout(() => {
+            createUpdateWindow.style.display = "flex";
+        }, 800);
+
+      };
+    }
+
+    renderAllTasks() {
+      const taskContainer = document.querySelector(".TasksList");
+      taskContainer.innerHTML = "";
+
+      const allTasks = this.databaseRead();
+
+      const niceBackground = [
+        "background-image: linear-gradient(120deg, #a6c0fe 0%, #f68084 100%);",
+        "background-image: linear-gradient(to right, #ffecd2 0%, #fcb69f 100%);",
+        "background-image: linear-gradient(to right, #ffecd2 0%, #fcb69f 100%);",
+        "background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);",
+      ];
+      let wheel = -1;
+
+      allTasks.map((item) => {
+        wheel = wheel + 1;
+        
+        if (wheel >= niceBackground.length) {
+          wheel = 0;
+        }
+        const colorToChoose = niceBackground[wheel];
+
+        const html = `   <div class="card ${item.id}" style="${colorToChoose}">
+                                    <div class="cardWrapper">
+                                        <p class="title">${item.title}</p>
+                                    </div>
+                                    <p>Read More.</p>
+
+                                    <div class="moreOptions"><button class="updateButton ${item.id}">update</button> <button class="deleteButton ${item.id}">delete</button></div>
+                                </div>
+                            `;
+        taskContainer.insertAdjacentHTML("afterbegin", html);
+      });
+    }
+
+    addTask() {
+      const createUpdateWindow = document.querySelector(".createUpdate");
+      const [keyNote, descriptionText] = document.querySelectorAll(".inputs");
+
+      let verify = descriptionText.value.split("");
+      let verify2 = keyNote.value.split("");
+
+      verify = verify.filter((x) => {
+        if (x !== " ") {
+          return x;
+        }
+      });
+
+      verify2 = verify2.filter((x) => {
+        if (x !== " ") {
+          return x;
+        }
+      });
+
+      if (verify.length !== 0 && verify2.length !== 0) {
+        // Unique Id
+        const id = this.createUniqueId();
+
+        // Creating the task object
+        const taskAndTitle = {
+          keyNote: keyNote,
+          descriptionText: descriptionText,
+          id: id,
+        };
+
+        this.databaseWrite(taskAndTitle);
+
+        keyNote.value = "";
+        descriptionText.value = "";
+
+        createUpdateWindow.style.display = "none";
+        
+        setTimeout(() => {
+            this.pageTransition();
+        }, 100);
+        
+        
+      } else {
+        alert("Please fill all fields");
+      }
+    }
+
+    removeTask(itemToRemove) {
+        const UniqueId = parseInt(itemToRemove[1]);
+        const oldData = this.databaseRead();
+        const newData = oldData.filter((item)=>{
+            return item.id !== UniqueId;
+        })
+        this.databaseUpdate(newData);
+        this.renderAllTasks();
+    }
+
+    updateTask(itemToUpdate) {
+        const that = this;
+        const createUpdateWindow = document.querySelector(".createUpdate");
+        const buttonToCreate = document.querySelector(".updateOrDelete");
+
+        let [keyNote, descriptionText] = document.querySelectorAll(".inputs");
+        const UniqueId= parseInt(itemToUpdate[1]);
+
+        let indexer = -1;
+
+        const allTasks = this.databaseRead();
+        const elementToUpdate = allTasks.filter((item)=>{return item.id === UniqueId});
+
+
+        keyNote.value = elementToUpdate[0].title;
+        descriptionText.value = elementToUpdate[0].task;
+
+        this.pageTransition();
+
+
+        buttonToCreate.onclick = function () {
+            let [keyNote, descriptionText] = document.querySelectorAll(".inputs");
+            elementToUpdate[0].title = keyNote.value;
+            elementToUpdate[0].task = descriptionText.value;
+            that.databaseUpdate(allTasks);
+            that.renderAllTasks();
+            that.pageTransition();
+            createUpdateWindow.style.display = "none";
+        }
+
+
+    }
+
+    sortTask() {}
+  }
+
+  const trigger = new TodoListHelper();
 })();
